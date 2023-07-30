0449d35f79cee172ae3bb072e195a93c8759205b
absormu <absoralvord07@gmail.com>
2023-07-30 21:37:16 +0700

    auto created_by & updated_by users

---
+++ app/Observers/PostObserver.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class PostObserver
+{
+    public function creating(User $user)
+    {
+        $user->created_by = Auth::user()->name;
+    }
+
+    public function updating(User $user)
+    {
+        $user->updated_by = Auth::user()->name;
+    }
+}
