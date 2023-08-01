52b5e4c910ee9498a43fd9c4607675b49b292589
absormu <absoralvord07@gmail.com>
2023-08-01 22:47:34 +0700

    report sales

--- config/app.php
+++ config/app.php
@@ -178,6 +178,9 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         App\Providers\FortifyServiceProvider::class,
+
+        Maatwebsite\Excel\ExcelServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
     ],
 
     /*
@@ -192,7 +195,8 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // ...
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
     ])->toArray(),
 
 ];
